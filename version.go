f3d387f906d8f91078c5eec9108b7e06af8c4316
lord-navy-crypto <haojunli283@gmail.com>
2026-08-30 14:07:48 +0800

    Make VERSION the single Sentinel version source

--- version.go
+++ version.go
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MPL-2.0
 package main
 
-const sentinelVersion = "2.2"
+import (
+	_ "embed"
+	"strings"
+)
+
+// VERSION is the single source of truth for the product version used by both
+// the Go engine and the macOS app bundle packaging scripts.
+//
+//go:embed VERSION
+var embeddedVersion string
+
+var sentinelVersion = strings.TrimSpace(embeddedVersion)
